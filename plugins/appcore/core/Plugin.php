c37a8fbbb77899dcf7ef840235d6c2bb1073c788
tommy8699 <tomashresko700@gmail.com>
2025-06-10 10:26:20 +0200

    Add appCore plugin (composer, Plugin.php, version.yaml)

---
+++ plugins/appcore/core/Plugin.php
@@ -0,0 +1,86 @@
+<?php namespace AppCore\Core;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Plugin Information File
+ *
+ * @link https://docs.octobercms.com/3.x/extend/system/plugins.html
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * pluginDetails about this plugin.
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'Core',
+            'description' => 'No description provided yet...',
+            'author' => 'AppCore',
+            'icon' => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * register method, called when the plugin is first registered.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * boot method, called right before the request route.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * registerComponents used by the frontend.
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'AppCore\Core\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * registerPermissions used by the backend.
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'appcore.core.some_permission' => [
+                'tab' => 'Core',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * registerNavigation used by the backend.
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'core' => [
+                'label' => 'Core',
+                'url' => Backend::url('appcore/core/mycontroller'),
+                'icon' => 'icon-leaf',
+                'permissions' => ['appcore.core.*'],
+                'order' => 500,
+            ],
+        ];
+    }
+}
