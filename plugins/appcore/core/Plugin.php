2f912494367a1226b1eeca236c5d7a18abc82e01
tommy8699 <tomashresko700@gmail.com>
2025-06-10 10:32:29 +0200

    Clean code Core

--- plugins/appcore/core/Plugin.php
+++ plugins/appcore/core/Plugin.php
@@ -17,70 +17,9 @@ class Plugin extends PluginBase
     {
         return [
             'name' => 'Core',
-            'description' => 'No description provided yet...',
+            'description' => 'Provides core utilities, enums, and shared classes used across multiple application plugins.',
             'author' => 'AppCore',
             'icon' => 'icon-leaf'
-        ];
-    }
-
-    /**
-     * register method, called when the plugin is first registered.
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * boot method, called right before the request route.
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * registerComponents used by the frontend.
-     */
-    public function registerComponents()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'AppCore\Core\Components\MyComponent' => 'myComponent',
-        ];
-    }
-
-    /**
-     * registerPermissions used by the backend.
-     */
-    public function registerPermissions()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'appcore.core.some_permission' => [
-                'tab' => 'Core',
-                'label' => 'Some permission'
-            ],
-        ];
-    }
-
-    /**
-     * registerNavigation used by the backend.
-     */
-    public function registerNavigation()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'core' => [
-                'label' => 'Core',
-                'url' => Backend::url('appcore/core/mycontroller'),
-                'icon' => 'icon-leaf',
-                'permissions' => ['appcore.core.*'],
-                'order' => 500,
-            ],
         ];
     }
 }
